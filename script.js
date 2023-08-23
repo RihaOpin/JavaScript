5e6bb63f3fe5c7f8e2a761cfd8825e48ba5417ce
Rihards <rihardsopincans@gmail.com>
2023-08-23 21:42:18 +0200

    add script file

---
+++ script.js
@@ -0,0 +1,16 @@
+
+console.log(4 +  '5')
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--incr);
+
+console.log(5%2)
+console.log(2*4 === '8')
+
+const isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose)
