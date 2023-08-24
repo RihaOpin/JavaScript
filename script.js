423cdc576733d335808211a789630934ea11a02c
Rihards <rihardsopincans@gmail.com>
2023-08-24 20:29:14 +0200

    first

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-cgggggggg
+const numberOfFilms = prompt('How much movies you already view?', '');
+
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('One of last viewed movie?', ''),
+      b = prompt('How good it was?', ''),
+      c = prompt('One of last viewed movie?', ''),
+      d = prompt('How good it was?', '');
+
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB)
