166296fe09fdb44a18bedb25c8cdd83dc2ac28df
Rihards <rihardsopincans@gmail.com>
2023-08-30 18:05:32 +0200

    first

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('How much movies you already view?', '');
+const numberOfFilms = +prompt('How much movies you already view?', '');
 
 const personalMoviesDB = {
     count: numberOfFilms,
@@ -8,14 +8,37 @@ const personalMoviesDB = {
     privat: false
 };
 
-const a = prompt('One of last viewed movie?', ''),
-      b = prompt('How good it was?', ''),
-      c = prompt('One of last viewed movie?', ''),
-      d = prompt('How good it was?', '');
+// const a = prompt('One of last viewed movie?', ''),
+//       b = prompt('How good it was?', ''),
+//       c = prompt('One of last viewed movie?', ''),
+//       d = prompt('How good it was?', '');
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+// personalMoviesDB.movies[a] = b;
+// personalMoviesDB.movies[c] = d;
+
+for(let i = 0; i < 2; i++) {
+    const a = prompt('One of last viewed movie?', ''),
+          b = prompt('How good it was?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50) { 
+    personalMoviesDB.movies[a] = b;
+    console.log('done!');
+    } else {
+        console.log('error!');
+        i--;
+    }
+
+}
+
+if(personalMoviesDB.count < 10){
+    console.log('viewed is not too much movies');
+}else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30){
+    console.log('you are regular viewer');
+}else if (personalMoviesDB.count >=30) {
+    console.log('You are kinoman')
+}else{
+    console.log('something went wrong')
+}
 
 console.log(personalMoviesDB)
-
 
